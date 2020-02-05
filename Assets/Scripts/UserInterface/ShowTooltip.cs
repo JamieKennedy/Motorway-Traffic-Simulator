a1f85f1f2e0161405040d3b5374eee07e003502f
Jamie <jamiekennedy97@gmail.com>
2020-02-05 13:06:12 +0000

    Added tooltips
    Added tooltips for each parameter and added functionality for tooltips to appear on hover

--- Assets/Scripts/UserInterface/ShowTooltip.cs
+++ Assets/Scripts/UserInterface/ShowTooltip.cs
@@ -5,30 +5,15 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 
 public class ShowTooltip : MonoBehaviour {
-    [SerializeField]
-    private string tooltipText;
-    [SerializeField]
-    private Vector3 position;
 
-    [SerializeField] private GameObject element;
+    [SerializeField] private GameObject toolTip;
 
-    [SerializeField] 
-    private GameObject toolTip;
-
-    private Vector3 tooltipPosition;
-
-    private void Awake() {
-        tooltipPosition = element.GetComponent<RectTransform>().position + new Vector3(
-                              element.GetComponent<RectTransform>().rect.width,
-                              -element.GetComponent<RectTransform>().rect.height / 2,
-                              0);
-    }
 
     public void Show(){
-        toolTip.GetComponent<Tooltip>().ShowTooltip(tooltipText, tooltipPosition);
+        toolTip.SetActive(true);
     }
 
     public void Hide() {
-        toolTip.GetComponent<Tooltip>().HideToolTip();
+        toolTip.SetActive(false);
     }
 }
