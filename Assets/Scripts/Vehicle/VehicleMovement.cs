88453fd45e32b72b5ee2124e117610a5c23bdf87
Jamie <jamiekennedy97@gmail.com>
2020-02-17 14:31:26 +0000

    Added vehicle properties for IDM

--- Assets/Scripts/Vehicle/VehicleMovement.cs
+++ Assets/Scripts/Vehicle/VehicleMovement.cs
@@ -4,25 +4,23 @@ using UnityEngine;
 
 public class VehicleMovement : MonoBehaviour {
     
-
-    private GameObject motorwayManager;
-    private Parameters motorwayParameters;
-
     private VehicleProperties vehicleProperties;
     
     // Start is called before the first frame update
     void Start() {
         vehicleProperties = gameObject.GetComponent<VehicleProperties>();
-        motorwayManager = GameObject.FindWithTag("MotorwayManager");
-        motorwayParameters = motorwayManager.GetComponent<Parameters>();
-        vehicleProperties.desiredSpeed =
-            Random.Range(motorwayParameters.speedLimit - 10, motorwayParameters.speedLimit + 10);
     }
 
     // Update is called once per frame
     void FixedUpdate() {
         if (vehicleProperties.canMove) {
-            gameObject.transform.position += new Vector3(vehicleProperties.desiredSpeed * 0.02f * (int) vehicleProperties.direction, 0 , 0);
+            gameObject.transform.position += new Vector3(vehicleProperties.currentVel * 0.02f * (int) vehicleProperties.direction, 0 , 0);
         }
+    }
+
+    public float IDM(GameObject vehicleA, GameObject vehicleB) {
+        var s = vehicleB.transform.position.x - vehicleA.transform.position.x;
+
+        return 0;
     }
 }
--- Assets/Scripts/Vehicle/VehicleProperties.cs
+++ Assets/Scripts/Vehicle/VehicleProperties.cs
@@ -4,13 +4,48 @@ using UnityEngine;
 
 public class VehicleProperties : MonoBehaviour {
     
-    [SerializeField] public float desiredSpeed;
+    private GameObject motorwayManager;
+    private Parameters motorwayParameters;
+    
+    public float desiredSpeed;
+    public int freeAccExponent;
+    public float desiredTimeGap;
+    public float jamDistance;
+    public float maxAccel;
+    public float desiredDecel;
+    public float politeness;
+
+    public float currentVel;
+    public float currentAccel;
+    
     public bool canMove = false;
     public int currentLane;
     public LaneProperties.direction direction;
     // Start is called before the first frame update
-    void Start() { }
+    void Start() {
+        motorwayManager = GameObject.FindWithTag("MotorwayManager");
+        motorwayParameters = motorwayManager.GetComponent<Parameters>();
+        
+        setParameters();
 
-    // Update is called once per frame
-    void Update() { }
+        currentVel = Random.Range(motorwayParameters.speedLimit * 0.8f, motorwayParameters.speedLimit * 1.2f);
+        currentAccel = 0f;
+    }
+
+    private void setParameters() {
+        desiredSpeed = Random.Range(motorwayParameters.speedLimit * 0.8f, motorwayParameters.speedLimit * 1.2f);
+
+        freeAccExponent = 4;
+
+        desiredTimeGap = Random.Range(1.5f * 0.8f, 1.5f * 1.2f);
+
+        jamDistance = Random.Range(2f * 0.8f, 2f * 1.2f);
+
+        maxAccel = Random.Range(1.4f * 0.8f, 1.4f * 1.2f);
+
+        desiredDecel = Random.Range(2f * 0.8f, 2f * 1.2f);
+
+        politeness = Random.Range(((motorwayParameters.politeness * 2) / 100) * 0.8f,
+            ((motorwayParameters.politeness * 2) / 100) * 1.2f);
+    }
 }
